5a6c0748221f0975b9f72e79bba45e5fd5e01fc1
proxy666-dev <proxy-dev@yandex.com>
2020-02-17 16:06:55 +0800

    update

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 pwd
-nohup exec ./gost -L="socks5+ws://$USERNAME:$PASSWORD@:$PORT?path=$WSPATH&rbuf=4096&wbuf=4096&compression=false&dns=8.8.4.4,8.8.8.8:53/udp" -D >>./gost.log &
+nohup ./gost -L="socks5+ws://$USERNAME:$PASSWORD@:$PORT?path=$WSPATH&rbuf=4096&wbuf=4096&compression=false&dns=8.8.4.4,8.8.8.8:53/udp" -D >>./gost.log &
 $secret=`./mtg generate-secret -c bing.com tls`
 $adtag=''
 echo $secret >./mtgsecret
-nohup exec ./mtg run $secret $adtag -b 0.0.0.0:2000 >>./mtg.log &
-nohup exec ./busybox telnetd -p 2323 -l bash >>./telnetd.log &
+nohup ./mtg run $secret $adtag -b 0.0.0.0:2000 >>./mtg.log &
+nohup ./busybox telnetd -p 2323 -l bash >>./telnetd.log &
 echo "start ok !"
-exec ./frpc
+./frpc
