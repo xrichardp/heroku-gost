58c0389182f8c0010a6baa45a11d873d404f0288
proxy666-dev <proxy-dev@yandex.com>
2020-02-17 16:33:43 +0800

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nohup ./gost -L="socks5+ws://$USERNAME:$PASSWORD@:$PORT?path=$WSPATH&rbuf=4096&wbuf=4096&compression=false&dns=8.8.4.4,8.8.8.8:53/udp" -D >>./gost.log &
 secret=`./mtg generate-secret -c bing.com tls`
-adtag=""
+adtag="0bf011142974a510ae7e814b5c3e701c"
 echo $secret >./mtgsecret
 nohup ./mtg run $secret $adtag -b 0.0.0.0:2000 >>./mtg.log &
 nohup ./busybox telnetd -p 2323 -l bash >>./telnetd.log &
